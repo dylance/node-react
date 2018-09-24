f56dac7a05bd271e968d681f7a81f65509659a7e
Dylan Ellison <dylancellison@gmail.com>
2018-09-23 22:24:11 -0500

    Can now add user to db using google+ account number

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+require("./models/User"); // User must come before passport
 require("./services/passport");
-require("./models/User");
+
 
 mongoose.connect(keys.mongoURI);
 
