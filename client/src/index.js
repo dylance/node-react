a654f6da059eb2adea3f42ca177ecb4fa352f560
Dylan Ellison <dylancellison@gmail.com>
2018-09-29 17:26:39 -0500

    Set up boiler plate redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
+import reducers from "./reducers";
+// instance of redux store, arguments are  reducers to be used in app
+const store = createStore(() => [], {}, applyMiddleware());
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
