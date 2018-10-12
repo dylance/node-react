dacc2524a27072c00559382138253cf33e31c925
Dylan Ellison <dylancellison@gmail.com>
2018-10-11 22:27:33 -0500

    Styled buttons

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -5,10 +5,16 @@ class Payments extends Component {
   render(){
     return (
       <StripeCheckout
+        name="Email App"
+        description="Pay for email credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_PUB_KEY}
-      />
+      >
+        <button className="btn">
+          Add Credits
+        </button>
+      </StripeCheckout>
     );
   }
 }
