6f77cb645718c944e42177cd1edaf8ad7bfd4863
Dylan Ellison <dylancellison@gmail.com>
2018-10-13 16:23:24 -0500

    Added users credits to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -15,6 +15,9 @@ class Header extends Component {
       default:
         return [
           <li key="1"><Payments /></li>,
+          <li key="3">
+            Credits: {this.props.auth.credits}
+          </li>,
           <li key="2"><a href="/api/logout">Log out</a></li>
         ];
     }
