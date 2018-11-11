a5cdf2ef5b0481e30db97b27649e8e64d2131a6c
Dylan Ellison <dylancellison@gmail.com>
2018-11-11 11:28:37 -0600

    set up sendgrid object to send emails

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,12 @@ import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
-
+// Code to test email send in browser
+import axios from "axios";
+window.axios = axios;
+// browser code
+// const survey = { title: 'my title', subject: 'my subbject', recipients: 'dylancellison@gmail.com, dylanellison35mm@gmail.com, dylan.ellison@fahrenheit.io', body: 'here is the body of the email'}
+// axios.post('/api/surveys', survey);
 
 // instance of redux store, arguments are  reducers to be used in app
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
