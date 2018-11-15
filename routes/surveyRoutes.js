e88d249015ab2080d6e8ade87d8ee17b97dd8354
Dylan Ellison <dylancellison@gmail.com>
2018-11-14 23:06:39 -0600

    handle web hooks from sendgrid

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const pathParser = require("path-parser");
+const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -7,6 +10,24 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const events = _.map(req.body, ({ email, url }) => {
+      const pathname = new URL(url).pathname;
+      const p = new pathParser.Path('/api/surveys/:surveyId/:choice');
+      const match = p.test(pathname);
+      console.log("match is", match)
+      console.log("email is ", email)
+      if (match) {
+        return { email, surveyId: match.surveyId, choice: match.choice }
+      }
+    });
+    console.log("events is: ", events);
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+    console.log("the uniqurie events are:", uniqueEvents);
+  });
+
   app.get('/api/surveys/thanks', (req, res) => {
     res.send('Thanks for voting!');
   })
